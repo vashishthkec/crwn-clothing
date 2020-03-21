36367e36bf43d5801389a7918ad412e115c77940
Pankaj Vashishth <pankaj@Pankajs-MacBook-Pro.local>
2020-03-22 01:42:06 +0530

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import {  Switch , Route } from 'react-router-dom';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component';
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
-  return <div>
-  <Switch>
-    <Route exact path='/' component={HomePage}/>
-    <Route path='/shop' component={ShopPage}/>
-    <HomePage/>
-  </Switch>
-    
-  </div>;
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <HomePage />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
