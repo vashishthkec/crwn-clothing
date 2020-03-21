c0cca77a0a815abac96102835e8d2acbbd697439
Pankaj Vashishth <pankaj@Pankajs-MacBook-Pro.local>
2020-03-21 13:26:31 +0530

    update menu-item to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+
+import {  Switch , Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return <div>
+  <Switch>
+    <Route exact path='/' component={HomePage}/>
+    <Route path='/hats' component={HatsPage}/>
     <HomePage/>
+  </Switch>
+    
   </div>;
 }
 
